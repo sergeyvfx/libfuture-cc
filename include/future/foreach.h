5c3087c1677aecd72dbb6776ee6676a7288d5455
Sergey Sharybin <sergey.vfx@gmail.com>
2015-05-14 17:06:00 +0500

    Simplify state machine in the foreach loop

--- include/future/foreach.h
+++ include/future/foreach.h
@@ -31,46 +31,32 @@ class foreach_helper {
  public:
   typedef typename T::const_iterator iterator_type;
   explicit foreach_helper(const T& range) : current_iterator_(range.begin()),
-                                            end_iterator_(range.end()),
-                                            value_loop_state_(LOOP_DONE) {}
+                                            end_iterator_(range.end()) {}
   inline bool is_done() {
-    return value_loop_state_ == LOOP_NEED_START ||
-           current_iterator_ == end_iterator_;
+    return current_iterator_ == end_iterator_;
   }
   inline void step() {
     ++current_iterator_;
   }
-
-  inline iterator_type& value_loop_setup() {
-    value_loop_state_ = LOOP_NEED_START;
+  inline iterator_type& current_iterator() {
     return current_iterator_;
   }
-  inline bool is_value_loop_done() {
-    return value_loop_state_ == LOOP_DONE;
-  }
-  inline void value_loop_step() {
-    value_loop_state_ = LOOP_DONE;
-  }
  protected:
-  enum loop_state {
-    LOOP_NEED_START = 0,
-    LOOP_DONE = 1,
-  };
   iterator_type current_iterator_;
   iterator_type end_iterator_;
-  loop_state value_loop_state_;
 };
 
 }  /* namespace internal */
 
 #if defined(__GNUC__) || defined(__clang__)
 #  define FUTURE_FOREACH(var, range) \
+  if (bool global_stop = false) {} else \
   for (future::internal::foreach_helper<__typeof__(range)> helper(range); \
-       !helper.is_done(); \
+       !helper.is_done() && !global_stop; \
        helper.step()) \
-  for (var = *helper.value_loop_setup(); \
-       !helper.is_value_loop_done(); \
-       helper.value_loop_step())
+  if(bool stop = false) {} else \
+  if(!(global_stop = true)) {} else \
+  for (var = *helper.current_iterator(); !stop; stop = true, global_stop = false)
 #else
 #  error "No foreach loop implementation for this compiler"
 #endif
